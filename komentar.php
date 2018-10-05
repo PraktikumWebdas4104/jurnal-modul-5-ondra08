e3bb4759f5c489c6f018011c38f25a828577d80e
Ondra <43498902+ondra08@users.noreply.github.com>
2018-10-05 10:25:12 +0700

    komentar.php
    Ondra Dwi Putra.A
    6701171121
    D3MI4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,15 @@
-<?php
+<form
+komentar :<input type="text" name="komentar">
+<input type="submit" name="submit" value="cek">
+</form>
 
+<?php
+if(isset($_POST['submit'])){
+$komentar=$_POST['komentar'];
+if(str_word_count($komentar)>=5){
+echo $komentar;
+}else if(str_word_count($komentar)<5){
+echo"error,kata sangat dari 5";
+}
+}
 ?>
